bd70a74744742e2c5d9b7599e5d8735a6106293d
GD Matto58 <mattothepou@gmail.com>
2020-05-11 14:46:45 +0200

    MattoTube v1
    It's finally here! Well, almost. I've actually deleted the older repository with the same name, because I renamed the folder with the files. You can get the main site here: https://www.newgrounds.com/portal/view/754879

---
+++ data/EN/data/scripts/channelVisit.js
@@ -0,0 +1,50 @@
+// YT channel names
+var Tom_Scott = 'https://www.youtube.com/channel/UCBa659QWEk1AI4Tg--mrJ2A'
+var PewDiePie = 'https://www.youtube.com/user/PewDiePie'
+var Matto58 = 'https://www.tinyurl.com/MattoSubs'
+var MarttoCZ = 'https://www.youtube.com/channel/UC4EoaqkJW4YuH8n-JO-S4YA'
+var Enki_hraje = 'https://www.youtube.com/channel/UCHmntWRkmGhwGnV-hVY-EMQ'
+var jacksfilms = 'https://www.youtube.com/user/jacksfilms'
+var ParashockX = 'https://www.youtube.com/channel/UCKUnB5P0cdfnufPCKkGecqQ'
+var GD_Colon = 'https://www.youtube.com/channel/UCFDsxSlQXpLLpVScy2NmbcQ'
+var Juniper = 'https://www.youtube.com/channel/UCOE3keaPI49vGbygNwsNqgA'
+var SirYakari = 'https://www.youtube.com/channel/UCggdHIILKfXpdB_0KI0Nmeg'
+var Danini09_Fantomas = 'https://www.youtube.com/channel/UCgSk_c42uq1LOXrgMCrQTZw'
+var addChannel = 'https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&source=mailto&su=Add+a+channel&to=mattothepou@gmail.com'
+// Button functions
+function visit(Tom_Scott) {
+  window.open(Tom_Scott)
+}
+function visit(PewDiePie) {
+  window.open(PewDiePie)
+}
+function visit(Matto58) {
+  window.open(Matto58)
+}
+function visit(MarttoCZ) {
+  window.open(MarttoCZ)
+}
+function visit(Enki_hraje) {
+  window.open(Enki_hraje)
+}
+function visit(jacksfilms) {
+  window.open(jacksfilms)
+}
+function visit(ParashockX) {
+  window.open(ParashockX)
+}
+function visit(GD_Colon) {
+  window.open(GD_Colon)
+}
+function visit(Juniper) {
+  window.open(Juniper)
+}
+function visit(SirYakari) {
+  window.open(SirYakari)
+}
+function visit(Danini09_Fantomas) {
+  window.open(Danini09_Fantomas)
+}
+function addChannel() {
+  window.open(addChannel)
+}
